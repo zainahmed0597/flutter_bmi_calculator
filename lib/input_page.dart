18508ae72bcd7b1eea2a4eef8c521035a676aad9
Zain Ahmed <48980361+zainahmed0597@users.noreply.github.com>
2019-09-30 11:02:39 +0500

    make UI

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class InputPage extends StatefulWidget {
@@ -10,11 +11,71 @@ class _InputPageState extends State<InputPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 0.0,
         centerTitle: true,
-        title: Text('BMI CALCULATOR'),
+        title: Text('BMI Calculator'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+              child: Row(
+            children: <Widget>[
+              Expanded(
+                child: ReusableCard(
+                  colour: Color(0xFF1D1E33),
+                ),
+              ),
+              Expanded(
+                child: ReusableCard(
+                  colour: Color(0xFF1D1E33),
+                ),
+              ),
+            ],
+          )),
+          Expanded(
+            child: ReusableCard(
+              colour: Color(0xFF1D1E33),
+            ),
+          ),
+          Expanded(
+              child: Row(
+            children: <Widget>[
+              Expanded(
+                child: ReusableCard(
+                  colour: Color(0xFF1D1E33),
+                ),
+              ),
+              Expanded(
+                child: ReusableCard(
+                  colour: Color(0xFF1D1E33),
+                ),
+              ),
+            ],
+          )),
+          Container(
+            color: Color(0xFFB1555),
+            margin: EdgeInsets.only(top: 10.0),
+            width: double.infinity,
+            height: 80.0,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({@required this.colour});
+
+  final Color colour;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        color: Color(0xFF1D1E33),
       ),
     );
   }
